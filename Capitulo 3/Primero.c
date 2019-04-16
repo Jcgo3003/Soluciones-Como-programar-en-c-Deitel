fcee99a3753a703ed2efebd69d4859a0ee598b7b
Juan Carlos Gomez <jcgo3003@outlook.com>
2019-04-16 19:03:00 +0200

    Create Primero.c
    Figura 2.1: fig02_01.c
    Un primer programa en c

---
+++ Capitulo 3/Primero.c
@@ -0,0 +1,13 @@
+/* Figura 2.1: fig02_01.c
+    un primer programa en c */
+
+#include<stdio.h>
+/* La funcion main indica la ejecucion de l programa*/
+
+int main(void)
+{
+    printf("\"Bienvenido a c!\"");
+    return 0;
+    /* Indica que el programa termino con exito*/
+}
+/* fin de la funcion main*/
